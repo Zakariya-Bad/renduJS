f8b5858f033a5f84924d6161bba8c19f34f3c8d7
Zakariya-Bad <zakariya.badache@latoile.em-lyon.com>
2025-12-16 11:13:11 +0100

    t

---
+++ script.js
@@ -0,0 +1,11 @@
+// Etape 1: HTML
+// -faire le header et mettre une nav avec le logo ("nomCommercial") les sections ( acceuil,avantages...) et un bouton appeler le tout dans une div
+// - dans le main ajouter une section hero qui va contenir les infos "phraseAccroche", "texteAppelAction"( bouton)
+// -ajouter une section avantages qui va contenir des cards en js avec les infos "avantagesClients" (h2,p)
+// -ajouter une section produits qui va contenir des cards en js avec les infos "produits" ( image ,h2,p)
+// -ajouter une section services qui va contenir des cards en js avec les infos "services" (h2,p)
+// -ajouter une section témoignages qui va contenir des cards en js avec les infos  "temoignages" (h2,3p)
+// -faire le footer 
+
+// Etape 1: JavaScript
+// -
