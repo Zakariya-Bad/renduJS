2dbb09ff78136a6a7b5519c01fa3ecc6aab11df7
Zakariya-Bad <zakariya.badache@latoile.em-lyon.com>
2025-12-16 13:06:17 +0100

    maj 1

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // Etape 1: HTML
 // -faire le header et mettre une nav avec le logo ("nomCommercial") les sections ( acceuil,avantages...) et un bouton appeler le tout dans une div
-// - dans le main ajouter une section hero qui va contenir les infos "phraseAccroche", "texteAppelAction"( bouton)
+// -dans le main ajouter une section hero qui va contenir les infos "phraseAccroche", "texteAppelAction"( bouton)
 // -ajouter une section avantages qui va contenir des cards en js avec les infos "avantagesClients" (h2,p)
 // -ajouter une section produits qui va contenir des cards en js avec les infos "produits" ( image ,h2,p)
 // -ajouter une section services qui va contenir des cards en js avec les infos "services" (h2,p)
 // -ajouter une section témoignages qui va contenir des cards en js avec les infos  "temoignages" (h2,3p)
 // -faire le footer 
 
-// Etape 1: JavaScript
-// -
+// Etape 2: JavaScript
+// -faire un fetch
+// -ajouter dans le JS la section logo 
+// -créer un p et ajouter le logo "nomCommercial" dans la section
+// -ajouter dans le JS la div hero-container 
+// -créer un h2 et ajouter "phraseAccroche" un button avec "texteAppelAction"
+// -Ajouter la div "card-avantages"
+// -créer une card qui contient titre avantage... et un h3 avec  "avantagesClients"
+// -Ajouter la div "card-produits"
+// -créer une card qui contient une img "image-url" , un h2 "nom" et un p "description"
+// -Ajouter la div "card-services"
+// -créer une card qui contient un h2 "nom" et un p "description"
+// -Ajouter la div "card-temoignages"
+// -créer une card qui contient un p  "prenom", un p "typeExperience", un p "commentaire" et un p "note"
+// -ajouter ces éléments à la page
+
+
+const apiURL= "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json"
+fetch(apiURL)
+  .then(response => response.json())
+  .then(data => { 
+    // code pour afficher les plats
+  });
